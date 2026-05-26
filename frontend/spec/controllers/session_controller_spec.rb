c5aa8c0a934fa64aa45f0a924de83099ed4ce826
lorawoodford <lorawoodford@gmail.com>
2026-05-26 16:35:21 -0400

    frontend session test

--- frontend/spec/controllers/session_controller_spec.rb
+++ frontend/spec/controllers/session_controller_spec.rb
@@ -202,56 +202,73 @@ describe SessionController, type: :controller do
     end
   end
 
-  describe "GET #check_pui_session" do
-    before(:each) do
-      set_repo($repo)
-      url = URI.parse(AppConfig[:backend_url] + '/users/admin/login')
-      request = Net::HTTP::Post.new(url.request_uri)
-      request.set_form_data('expiring' => 'false',
-                            'password' => 'admin')
-      response = do_http_request(url, request)
-      JSONModel::HTTP.current_backend_session = ASUtils.json_parse(response.body)['session']
+  describe '#logout' do
+    context 'when pui_require_authentication is enabled and public_proxy_url is set' do
+      before(:each) do
+        allow(AppConfig).to receive(:[]).and_call_original
+        allow(AppConfig).to receive(:[]).with(:pui_require_authentication).and_return(true)
+        allow(AppConfig).to receive(:has_key?).and_call_original
+        allow(AppConfig).to receive(:has_key?).with(:public_proxy_url).and_return(true)
+        allow(AppConfig).to receive(:[]).with(:public_proxy_url).and_return('http://localhost:3001')
+
+        @mock_http = instance_double(Net::HTTP)
+        @mock_request = instance_double(Net::HTTP::Post)
+
+        allow(Net::HTTP).to receive(:new).and_call_original
+        allow(Net::HTTP).to receive(:new).with('localhost', 3001).and_return(@mock_http)
+        allow(Net::HTTP::Post).to receive(:new).and_call_original
+        allow(Net::HTTP::Post).to receive(:new).with('/logout_staff_session').and_return(@mock_request)
+        allow(@mock_request).to receive(:[]=)
+        allow(@mock_http).to receive(:request)
+      end
+
+      it 'resets the session and redirects to root' do
+        session = User.login('admin', 'admin')
+        User.establish_session(controller, session, 'admin')
+
+        delete :logout
+
+        expect(controller.session[:user]).to be_nil
+        expect(response).to redirect_to('/')
+      end
+
+      it 'attempts to notify the public app of the logout' do
+        delete :logout
+
+        expect(@mock_http).to have_received(:request).with(@mock_request)
+      end
     end
 
-    context "with session with permissions" do
+    context 'when pui_require_authentication is disabled' do
       before(:each) do
-        user = build(:json_user).save(password: '123')
-        user = User.find(user)
-        group = create(:json_group,
-                       member_usernames: [user.username],
-                       grants_permissions: ['view_repository', 'view_pui'])
-        session = User.login(user.username, '123')
-        User.establish_session(controller, session, user.username)
+        allow(AppConfig).to receive(:[]).and_call_original
+        allow(AppConfig).to receive(:[]).with(:pui_require_authentication).and_return(false)
       end
 
-      it "returns a username, session, and view pui is true" do
-        response = get :check_pui_session, params: { }
-        parsed_response = JSON.parse(response.body)
+      it 'resets the session and redirects to root without calling public app' do
+        session = User.login('admin', 'admin')
+        User.establish_session(controller, session, 'admin')
 
-        expect(parsed_response['username']).not_to be_nil
-        expect(parsed_response['session']).not_to be_nil
-        expect(parsed_response['view_pui']).to be_truthy
+        delete :logout
+
+        expect(controller.session[:user]).to be_nil
+        expect(response).to redirect_to('/')
+        expect(Net::HTTP).not_to receive(:new).with('localhost', 3001)
       end
     end
+  end
 
-    context "with session but no permissions" do
+  describe '#check_pui_session' do
+    context 'when pui_require_authentication is disabled' do
       before(:each) do
-        user = build(:json_user).save(password: '456')
-        user = User.find(user)
-        group = create(:json_group,
-                       member_usernames: [user.username],
-                       grants_permissions: [])
-        session = User.login(user.username, '456')
-        User.establish_session(controller, session, user.username)
+        allow(AppConfig).to receive(:[]).and_call_original
+        allow(AppConfig).to receive(:[]).with(:pui_require_authentication).and_return(false)
       end
 
-      it "returns a username, session, and view pui is false" do
-        response = get :check_pui_session, params: { }
-        parsed_response = JSON.parse(response.body)
+      it 'returns 403' do
+        get :check_pui_session
 
-        expect(parsed_response['username']).not_to be_nil
-        expect(parsed_response['session']).not_to be_nil
-        expect(parsed_response['view_pui']).not_to be_truthy
+        expect(response.status).to eq(403)
       end
     end
   end
